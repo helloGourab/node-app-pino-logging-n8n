973296b52366082cb83eb2d0ea90b6f74766f192
helloGourab <gourab.m099@gmail.com>
2026-03-14 11:51:19 +0530

    bug: seed savedData.stargazers_count undefined err in gh_controller

--- src/controllers/github.controller.js
+++ src/controllers/github.controller.js
@@ -8,7 +8,12 @@ export const importRepo = async (req, res, next) => {
         // We keep this info log because it's a "Happy Path" milestone
         req.log.info({ msg: "GITHUB_REPO_IMPORTED", id: savedData._id });
 
-        res.status(201).json(savedData);
+        const formattedResponse = {
+            ...savedData.toObject(),
+            starBadge: savedData.stargazers_count.toLocaleString() + ' ⭐'
+        };
+
+        res.status(201).json(formattedResponse);
     } catch (error) {
         // Hand off to the global handler for n8n notification and 500 response
         next(error);
